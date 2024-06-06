53f37aea7fbaf96c0d371d189e1b34fb96219dec
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-06 13:19:54 +0600

    swiper silder and spinner added to the projects

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className='text-center'>
+            <span className="loading loading-bars loading-lg"></span>
+        </div>
+    );
+};
+
+export default Spinner;
