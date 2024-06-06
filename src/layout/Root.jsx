1ffb9f79e06248730f23378326ff7ae31973a2a9
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-06 13:15:51 +0600

    layout of the project is done

---
+++ src/layout/Root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../shared/Navbar/Navbar';
+import Footer from '../shared/Footer/Footer';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Root = () => {
+    return (
+        <div className=' max-w-sm md:max-w-3xl lg:max-w-7xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+            <ToastContainer/>
+        </div>
+    );
+};
+
+export default Root;
