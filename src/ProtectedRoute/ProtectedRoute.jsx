87f5592b0487559efbac7ca09ea69d225c617cb7
jubaerahmedtamim <jubaerahmedtamim2014@gmail.com>
2024-06-06 13:18:26 +0600

    protected router has been added

---
+++ src/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../provider/AuthProvider';
+import { Navigate, useLocation } from 'react-router-dom';
+import Spinner from '../components/Spinner/Spinner';
+
+const ProtectedRoute = ({children}) => {
+    const {user,loading} = useContext(AuthContext);
+    const location = useLocation();
+    const from = location.pathname;
+    console.log(location);
+
+    if(loading){
+        return <Spinner></Spinner>
+    }
+
+    if(user){
+        return children;
+    }
+    return <Navigate state={from} to='/login'></Navigate>
+};
+
+export default ProtectedRoute;
